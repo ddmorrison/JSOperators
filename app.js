a8ee632068eafbffdf690f1b12d77c1e1ef84e2c
ddmorrison <danidelemorr26@outlook.com>
2021-12-27 11:29:50 -0600

    JSOperator

---
+++ app.js
@@ -0,0 +1,55 @@
+console.log("Hello World!\n==========\n");
+console.log(
+  "Follow the steps in the README.md file to complete the exercises:\n==========\n"
+);
+
+// Exercise 1
+console.log("EXERCISE 1:\n==========\n");
+let a = 20;
+let b = 4;
+let add = a + b;
+let minus = a - b;
+let multiply = a * b;
+let diving = a / b;
+console.log(a);
+console.log(b);
+console.log(add);
+console.log(minus);
+console.log(multiply);
+console.log(diving);
+
+// Exercise 2
+console.log("EXERCISE 2:\n==========\n");
+
+let c = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Jackie";
+let lastName = "Chan";
+
+console.log(c + str);
+console.log(c + str2);
+console.log(c + isPresent);
+console.log(c + firstName);
+console.log(c + lastName);
+
+// Exercise 3
+console.log("EXERCISE 3:\n==========\n");
+
+let d = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
+let firstname2 = "Robin";
+let lastName2 = "Williams";
+
+console.log(d == str3);
+console.log(d === str4);
+console.log(!isPresent2);
+console.log("eleven" == str3 && d >= str4);
+console.log(!isPresent2 || isPresent2);
+console.log(0 == false);
+console.log(0 === false);
+console.log(0 != false);
+console.log(0 !== false);
